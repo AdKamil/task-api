9147fb59388cd76f90bfaa35171f73b9c650c52f
Kamil Grząba <kamil.grzaba@gmail.com>
2019-06-11 22:51:25 +0200

    sequelize init

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from  'express';
+import graphqlHTTP from 'express-graphql';
+import { makeExecutableSchema } from 'graphql-tools';
+
+import resolvers from './resolvers';
+import typeDefs from './schema';
+import models from './models';
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP( req => ({
+  schema,
+  context: {
+     models,
+  },
+  graphiql: true,
+})));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
